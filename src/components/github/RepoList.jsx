1fbc11aaa466b5b4928c11615769f4e74e6c8670
Carlos Espinal <cespinalt@gmail.com>
2016-10-08 00:48:13 -0700

    Render all user's repository names and description
    This component gets all repos from it's props and maps them calling
    the Repo component for each repository

--- src/components/github/RepoList.jsx
+++ src/components/github/RepoList.jsx
@@ -1,1 +1,16 @@
 import React, {Component} from 'react';
+import Repo from './Repo.jsx';
+
+class RepoList extends Component {
+  render() {
+    return(
+      <ul className="list-group">
+        {this.props.userRepo.map(repo => {
+          return <Repo repo={repo} />
+        })}
+      </ul>
+    )
+  }
+}
+
+export default RepoList;
